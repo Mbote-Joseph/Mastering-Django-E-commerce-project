696e52811e71e005aed5d14d8418dd02347f7fca
Mbote-Joseph <60655465+Mbote-Joseph@users.noreply.github.com>
2026-01-10 21:23:02 +0300

    Commenting out debugger

--- .history/storefront/urls_20260110212153.py
+++ .history/storefront/urls_20260110212153.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 admin.site.site_header = "Storefront Admin"
-admin.site.site_title = "Admin Storefront"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
